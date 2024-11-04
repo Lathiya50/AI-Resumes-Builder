3c9246c8a4bc7aceb1e68010727c2128ed3e2a7f
Lathiya50 <lathiyaharmik50@gmail.com>
2024-11-04 14:01:09 +0530

    build fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 import dotenv from "dotenv";
 dotenv.config();
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone", // Add this for better Vercel deployment
+};
 
 export default nextConfig;
